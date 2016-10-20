043731579ab11cd165cdd57773f5342fd5784425
Patrik Hrmo <patohrmo@gmail.com>
2016-10-20 10:18:04 +0200

    bst fully implement + tested

--- src/main/java/bsttree/BstTree.java
+++ src/main/java/bsttree/BstTree.java
@@ -1,16 +1,20 @@
 package bsttree;
 
+import java.util.Stack;
+
 public class BstTree<K extends Comparable<K>, T> {
 	private BstNode<K, T> root;
+	private int size;
 
 	public BstTree() {
 		root = null;
+		size = 0;
 	}
 
 	public boolean insert(K key, T newData) {
 		if (root == null) {
 			root = new BstNode<>(key, newData);
-			;
+			size++;
 			return true;
 		}
 		BstNode<K, T> actualNode = root;
@@ -18,6 +22,7 @@ public class BstTree<K extends Comparable<K>, T> {
 			if (actualNode.compareKeys(key) < 0) {
 				if (actualNode.left == null) {
 					actualNode.left = new BstNode<>(key, newData);
+					size++;
 					return true;
 				} else {
 					actualNode = actualNode.left;
@@ -27,6 +32,7 @@ public class BstTree<K extends Comparable<K>, T> {
 			if (actualNode.compareKeys(key) > 0) {
 				if (actualNode.right == null) {
 					actualNode.right = new BstNode<>(key, newData);
+					size++;
 					return true;
 				} else {
 					actualNode = actualNode.right;
@@ -70,13 +76,16 @@ public class BstTree<K extends Comparable<K>, T> {
 				if (actual.getNumberofKids() == 0) {
 					if (actual == root) {
 						root = null;
+						size--;
 						return actual.getData();
 					} else {
 						if (isLeftKid) {
 							parent.left = null;
+							size--;
 							return actual.getData();
 						} else {
 							parent.right = null;
+							size--;
 							return actual.getData();
 						}
 					}
@@ -85,40 +94,69 @@ public class BstTree<K extends Comparable<K>, T> {
 					if (actual == root) {
 						if (actual.left == null) {
 							root = actual.right;
+							size--;
 							return actual.getData();
 						}
 						if (actual.right == null) {
 							root = actual.left;
+							size--;
 							return actual.getData();
 						}
 					}
-					if (isLeftKid) {
+					else if (isLeftKid) {
 						if (actual.left == null) {
 							parent.left = actual.right;
+							size--;
 							return actual.getData();
 						}
 						if (actual.right == null) {
 							parent.left = actual.left;
+							size--;
 							return actual.getData();
 						}
 					} else {
 						if (actual.left == null) {
 							parent.right = actual.right;
+							size--;
 							return actual.getData();
 						}
 						if (actual.right == null) {
 							parent.right = actual.left;
+							size--;
 							return actual.getData();
 						}
 					}
 				}
 				if (actual.getNumberofKids() == 2) {
-					if (isLeftKid) {
-
+					if(actual == root) {
+						BstNode<K, T> successor = getSuccessorAndDeleteItFromParent(actual);
+						successor.left = root.left;
+						successor.right = root.right;
+						root = successor;
+						size--;
+						return actual.getData();
+					}
+					// deleted node is left kid of parent
+					else if (isLeftKid) {
+						BstNode<K, T> successor = getSuccessorAndDeleteItFromParent(actual);
+						successor.left = actual.left;
+						successor.right = actual.right;
+						parent.left = successor;
+						size--;
+						return actual.getData();
+					} 
+					// deleted node is right kid of parent
+					else {
+						BstNode<K, T> successor = getSuccessorAndDeleteItFromParent(actual);
+						successor.left = actual.left;
+						successor.right = actual.right;
+						parent.right = successor;
+						size--;
+						return actual.getData();
 					}
 				}
 			}
-			if (actual.compareKeys(key) < 0) {
+			else if (actual.compareKeys(key) < 0) {
 				if (actual.left == null)
 					return null;
 				else {
@@ -127,7 +165,7 @@ public class BstTree<K extends Comparable<K>, T> {
 					isLeftKid = true;
 				}
 			}
-			if (actual.compareKeys(key) > 0) {
+			else if (actual.compareKeys(key) > 0) {
 				if (actual.right == null)
 					return null;
 				else {
@@ -138,11 +176,51 @@ public class BstTree<K extends Comparable<K>, T> {
 			}
 		}
 	}
+	private BstNode<K, T> getSuccessorAndDeleteItFromParent(BstNode<K, T> node) {
+		BstNode<K, T> successor = node.right;
+		BstNode<K, T> successorParent = node;
+		
+		while(successor.left != null){
+			
+			successorParent = successor;
+			successor = successor.left;
+		}
+		// succesor ma na pravo deti
+		if(successor.right != null) {
+			//a je hned na pravo
+			if(successorParent==node) {
+				successorParent.right = successor.right;
+				return successor;
+			}
+			//a nie je hned na pravo
+			else {
+				successorParent.left = successor.right;
+				successor.right = null;
+				return successor;
+			}
+		}
+		// successor nema na pravo deti
+		else {
+			// a je hned na pravo
+			if(successorParent==node) {
+				successorParent.right = null;
+				return successor;
+			}
+			//a nie je hned na pravo
+			else {
+				successorParent.left = null;
+				return successor;
+			}
+		}
+	}
+	public int getSize() {
+		return this.size;
+	}
 
 	private void inOrder(BstNode<K, T> node) {
 		if (node != null) {
 			this.inOrder(node.left);
-			System.out.println(node.toString() + System.lineSeparator());
+			//System.out.println(node.toString() + System.lineSeparator());
 			this.inOrder(node.right);
 		}
 	}
@@ -151,5 +229,29 @@ public class BstTree<K extends Comparable<K>, T> {
 		if (root == null)
 			return;
 		this.inOrder(root);
+	}
+     T[] toArray(T[] pole){
+		if(root==null) return null;
+		int i = 0;
+		Stack<BstNode<K,T>> zasobnik  = new Stack<>();
+		BstNode<K, T> actual = root;
+		while(actual!=null) {
+			zasobnik.push(actual);
+			actual = actual.left;
+		}
+		while(zasobnik.size()>0) {
+			actual = zasobnik.pop();
+			pole[i] = actual.getData();
+			i++;
+			if(actual.right!=null) {
+				actual = actual.right;
+				while(actual!=null) {
+					zasobnik.push(actual);
+					actual = actual.left;
+				}
+			}
+			
+		}
+		return pole;
 	}
 }
---
+++ src/test/java/bsttree/BstTreeTest.java
@@ -0,0 +1,61 @@
+package bsttree;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Test;
+
+import pojo.Car;
+
+public class BstTreeTest {
+
+	
+
+	@Test
+	public void testAllOperations() {
+		for(int i = 0; i< 500;i++) {
+			System.out.println("aktualny seed je: "+i);
+			Random generator = new Random(i);
+			BstTree<Integer, Car> strom = new BstTree<>();
+			List<Car> auta = new ArrayList<>();
+			int size = 0;
+				for(int j=0; j<10000;j++) {
+					
+					if(generator.nextInt(1000)<800) {
+						Car auto = new Car(generator.nextInt(), generator.nextInt()+"");
+						if(strom.insert(auto.getEC(), auto)) {
+							auta.add(auto);
+							size++;
+						}
+					} else {
+						int pocetPrvkov = auta.size();
+						if(pocetPrvkov==0) continue;
+						Car auto = auta.remove(0);
+						strom.delete(auto.getEC());
+						size--;
+					}
+				}
+				strom.inOrder();
+				assertEquals("nie su rovako velke", size, strom.getSize());
+				auta.sort(new Comparator<Car>() {
+
+					@Override
+					public int compare(Car o1, Car o2) {
+						if(o1.getEC()==o2.getEC()) return 0;
+						if(o1.getEC()<o2.getEC()) return 1;
+						else return -1;
+					}
+				});
+				Car autaExpected[] = new Car[auta.size()];
+				auta.toArray(autaExpected);
+				Car autaActual[] = new Car[strom.getSize()];
+				strom.toArray(autaActual);
+				assertArrayEquals("nie su rovnake", autaExpected, autaActual);
+		}
+	}
+
+}
